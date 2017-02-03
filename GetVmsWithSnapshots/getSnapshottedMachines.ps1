737aab3930079d06d8ff24d37d57a0a56559de19
Sven De Preter <Sven.De.Preter@sportpaleis.net>
2017-02-03 10:32:26 +0100

    getVmsWithSnapshots, initial commit

---
+++ GetVmsWithSnapshots/getSnapshottedMachines.ps1
@@ -0,0 +1,7 @@
+﻿
+connect-viserver -server "spg-virtualcenter"
+ get-vm | get-snapshot |select  VM,Name,
+ 				@{Name="SizeGB";Expression={[math]::Round($_.SizeGB,2)}},
+				Created,
+				@{Name="Days Old";Expression= {(New-TimeSpan -End (get-date) -start $_.Created).Days}} | ft -AutoSize
+disconnect-viserver  -server "spg-virtualcenter" -Force -Confirm:$false
